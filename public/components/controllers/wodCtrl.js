ceef68620495daebfa686cdd7b981ca82f7ec650
Mike <mikeytown19@gmail.com>
2016-05-16 16:54:22 -0600

    added the toggleing for commenting

--- public/components/controllers/wodCtrl.js
+++ public/components/controllers/wodCtrl.js
@@ -1,6 +1,6 @@
 angular.module("crossfit").controller("wodCtrl", function($scope, wodService, settingsService) {
 
-    settingsService.getCurrentUser().then(function(res){
+    settingsService.getCurrentUser().then(function(res) {
 
         $scope.currentUser = res.data
         $scope.admin = res.data.admin
@@ -8,21 +8,26 @@ angular.module("crossfit").controller("wodCtrl", function($scope, wodService, se
     })
 
 
-        $scope.getAllWods = wodService.getAllWods().then(function(res){
-                $scope.allWods = res.data;
+    $scope.getAllWods = wodService.getAllWods().then(function(res) {
+        $scope.allWods = res.data;
+    })
+
+
+    $scope.custom = true;
+        $scope.toggleComments = function(){
+            $scope.custom = $scope.custom === false ? true: false;
+
+        }
+
+
+    $scope.postComment = function(userId, postId, newComment, showIndex) {
+        wodService.postComment(userId, postId, newComment).then(function(res) {
+            wodService.getAllWods()
         })
-
-
-
-$scope.postComment = function (userId, postId, newComment, showIndex) {
-   wodService.postComment(userId, postId, newComment).then(function(res) {
-       wodService.getAllWods()
-   })
- }
+    }
 
     $scope.deleteComment = function(id, dailyId) {
         wodService.deleteComment(id, dailyId);
     }
-
 
 });
